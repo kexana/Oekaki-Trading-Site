dd5eecfb0c709a6032d9a363e6ee123e23f2cb68
kexana <deotsts@gmail.com>
2022-02-20 19:16:44 +0200

    DrawFeature-Init

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace OekakiTradingSite.Controllers
             return View();
         }
 
+        public IActionResult Draw()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
